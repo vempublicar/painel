e91a59d2251d81e7041f27d0e3bfd9a968335e01
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-02-19 15:03:43 -0300

    1

---
+++ app/public/inicio.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Fepacoc | Registro</title>
+    <!-- CSS files -->
+    <link rel="icon" type="image/x-icon" href="vendor/images/fepacoc/icone.png" />
+    <link href="vendor/css/tabler.min.css" rel="stylesheet" />
+    <link href="vendor/css/tabler-flags.min.css" rel="stylesheet" />
+    <link href="vendor/css/tabler-payments.min.css" rel="stylesheet" />
+    <link href="vendor/css/tabler-vendors.min.css" rel="stylesheet" />
+    <link href="vendor/css/landingpage.css" rel="stylesheet" />
+    <link href="vendor/css/demo.min.css" rel="stylesheet" />
+    <style>
+        @import url('https://rsms.me/inter/inter.css');
+
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
+
+        body {
+            font-feature-settings: "cv03", "cv04", "cv11";
+        }
+    </style>
+</head>
+
+
+<body class="layout-fluid">
+    <div class="scroll-container">
+        <header class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark py-3">
+            <div class="container">
+                <a class="navbar-brand" href="inicio">
+                    <img src="vendor/images/fepacoc/logobranco_fepacoc.png" alt="Logo FEPACOC" class="navbar-brand-image">
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+
+                <div class="d-flex align-items-center">
+                    <a class="btn btn-outline-light" href="login">Acessar Painel</a>
+                </div>
+            </div>
+        </header>
+        <section class="scroll-section hero d-flex flex-column align-items-center justify-content-center text-center text-white" id="section0">
+            <div class="container">
+                <h1 class="display-4 mt-5">Facilite sua <b class="text-info">gestão empresarial</b><br> através de análises integradas.</h1>
+                <p class="lead mb-4">O FEPACOC é um método que simplifica a análise da sua empresa. Informando apenas os seus resultados brutos, 
+                    nossa ferramenta cruza informações estratégicas para gerar indicadores importantes. Com isso, conseguimos retornar as ações necessárias 
+                    para você alcançar suas metas.</p>
+                
+            </div>
+        </section>
+        <section class="scroll-section metodo-secao" id="section1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-10 mb-4 mx-auto d-flex flex-column align-items-center justify-content-center text-center">
+                        <h2 class="text-center p-1">Seus resultados mensais <br>transformados em <b class="text-primary">métricas</b>, <br> <b class="text-primary">objetivos</b> e <b class="text-primary">crescimento</b>.</h2>
+                    </div>
+                    <div class="col-xl-6 mx-auto col-9 mb-4">
+                        <div class="">
+                            <div class="card-body p-0 d-flex flex-column align-items-center justify-content-center text-center">
+                                <img src="vendor/images/template/funil.png" height="250" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="scroll-section metodo-secao cinza" id="section2">
+            <div class="container">
+                <div class="col text-center"><img src="vendor/images/template/fepacoc-funciona.png" height="200" alt=""></div>
+                <h2 class="text-center">"Como funciona?"</h2>
+                <p class="text-center mb-4">O método visualiza a empresa através de seis setores-chave, classificados em dois <br>grupos de análise:<span class="badge bg-orange text-orange-fg"> Interno </span> e <span class="badge bg-blue text-blue-fg"> Externo</span>. 
+                Os dados brutos de cada setores-chave são cruzados para revelar as <u>oportunidades do negócio</u>. </p>
+                <!-- <br>Esse processo gera indicadores essenciais,  que podem orientar o gestor em ações assertivas, criando previsibilidade nos resultados futuros. -->
+            
+            </div>
+        </section>
+        <section class="scroll-section hero   text-white" id="section3">
+            <div class="container">
+                <div class="row ">
+                    <div class="col-sm-5 mx-auto">
+                        <h2 class="">Grupo <b class="text-orange" >Interno</b></h2>
+                        <p class="lead mb-4 ">Representa as operações sob controle do gestor, permitindo ações assertivas e alinhamento com líderes de departamentos e diretoria.</p>
+                        <hr>
+                    </div>
+                    <div class="col-sm-5 mx-auto">
+                        <h2 class="">Grupo <b class="text-primary" >Externo</b></h2>
+                        <p class="lead mb-4 ">Representa as operações fora do controle direto do gestor, mas que exigem compreensão e ação quando possível, para alinhamento com equipe, fornecedores e terceiros.</p>
+                        <hr>
+                    </div>
+                </div>
+                <!-- <div class="row ">
+                    <div class="col-lg-4 col-md-6 mb-2">
+                        <div class="card p-3 h-100 shadow">
+                            <div class="ribbon ribbon-top bg-orange"></div>
+                            <h4 class="text-center">Financeiro</h4>
+                            <p class=" text-center text-secondary mb-2">Gerencie receitas, despesas, ativos e passivos, enquanto acompanha indicadores de KPIs e eficiência operacional para maximizar o valuation e garantir a saúde financeira.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-2">
+                        <div class="card p-3 h-100 shadow">
+                            <div class="ribbon ribbon-top bg-orange"></div>
+                            <h4 class="text-center">Estrutura</h4>
+                            <p class=" text-center text-secondary mb-2">Otimize a avaliação de funcionários, desempenho e estratégias, alinhando infraestrutura e marketing interno para elevar a performance e eficiência da empresa.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-2">
+                        <div class="card p-3 h-100 shadow">
+                            <div class="ribbon ribbon-top bg-orange"></div>
+                            <h4 class="text-center">Produto</h4>
+                            <p class="text-center text-secondary mb-2">Desenvolva e gerencie produtos com segmentação precisa, estratégias de precificação e parcerias, aumentando a percepção de valor e a eficácia das soluções.</p>
+                        </div>
+                    </div>
+                </div> -->
+            </div>
+        </section>
+        <!-- <section class="mt-7 mb-7 metodo-secao " id="section4">
+            <div class="container">
+                <div class="col-sm-6 mx-auto">
+                    <h2 class="text-center">Grupo <b class="text-primary" >Externo</b></h2>
+                    <p class="lead mb-4 text-center">Representa as operações fora do controle direto do gestor, mas que exigem compreensão e ação quando possível, para alinhamento com equipe, fornecedores e terceiros.</p>
+                    <hr>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 mb-2">
+                        <div class="card p-3 h-100 shadow">
+                            <div class="ribbon ribbon-top bg-primary"></div>
+                            <h4 class="text-center">Anúncio</h4>
+                            <p class="text-secondary text-center mb-2">Planeje e execute estratégias de marketing e comunicação, utilizando redes sociais, tráfego pago e criativos eficazes, com as ferramentas e estratégias certas para promover a marca.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-2">
+                        <div class="card p-3 h-100 shadow">
+                            <div class="ribbon ribbon-top bg-primary"></div>
+                            <h4 class="text-center">Clientes</h4>
+                            <p class="text-secondary text-center mb-2">Compreenda e gerencie atendimentos, otimize o funil de clientes e impulsione as vendas com ferramentas eficientes e treinamentos voltados para resultados.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-2">
+                        <div class="card p-3 h-100 shadow">
+                            <div class="ribbon ribbon-top bg-primary"></div>
+                            <h4 class="text-center">Operacional</h4>
+                            <p class="text-secondary text-center mb-2">Analise e otimize processos, aprimorando a cultura, hábitos e incentivos, avaliando terceiros, departamentos e prepare reuniões com estratégias de impacto.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section> -->
+        <section class="scroll-section metodo-secao cinza" id="section5">
+            <div class="row">
+                <div class="col-xl-4 col-12  mx-auto d-flex flex-column align-items-center justify-content-center text-center">
+                    <h2 class="text-center p-1">Todo gerenciamento através de <b class="text-primary">graficos interativos</b>.</h2>
+                </div>
+                <div class="col-xl-6 col-12 ">
+                    <div class="">
+                        <div class="card-body p-0 d-flex flex-column align-items-center justify-content-center text-center">
+                            <img src="vendor/images/template/tela-financeiro.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="scroll-section metodo-secao" id="section6">
+            <div class="col-md-5 mb-4 mx-auto d-none d-xl-block">
+                <div class="card-body p-0">
+                    <div id="radarChart" class="mt-2"></div>
+                </div>
+            </div>
+            <div class="col-md-4 mx-auto mb-4 d-none d-xl-block">
+                <h2 class="text-center">Mapa de performance para entender qual <b class="text-primary">área melhorar</b>.</h2>
+            </div>
+            <div class="d-block d-xl-none">
+                <div class="row">
+                    <div class="col-xl-4 mb-4 p-1 mx-auto d-flex flex-column align-items-center justify-content-center text-center">
+                        <h2 class="text-center">Mapa de performance para entender qual <b class="text-primary">área melhorar</b>.</h2>
+                    </div>
+                    <div class="col-xl-5 mb-4 mx-auto">
+                        <div class="card-body p-0 d-flex flex-column align-items-center justify-content-center text-center">
+                            <img src="vendor/images/template/mapa de desempenho.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>        
+        <section class="scroll-section hero  text-center text-white" id="section8">
+            <div class="container">
+                <h1 class="display-4 mt-5">Garanta a <b class="text-info">mudança</b><br> da sua empresa em 90 dias.</h1>
+                <p class="lead mb-4">Simplesmente organizando e cruzando os dados que sua empresa já possui,<br> você pode identificar os caminhos certos para o crescimento saudável e garantir um negócio lucrativo. <br>Deixe o método FEPACOC te ajudar com isso.</p>
+                <div class="row">
+                    <div class="col-sm-4 mx-auto">
+                        <a href="https://fepacoc.com.br" target="_blank"  class="btn btn-secondary btn-lg w-100 mt-5 py-2 mb-8">
+                            Conhecer Mais
+                        </a>
+                    </div>
+                    <div class="col-sm-4 mx-auto">
+                        <a href="planos" id="cadastro-btn" class="btn btn-info btn-lg btn-cadastro w-100 mt-5 py-2 mb-8">
+                            Ver os Preços
+                        </a>
+                    </div>                
+                </div>
+            </div>
+        </section>
+        <section class="scroll-section metodo-secao" id="section7">
+            <div class="container">
+                <div class="col text-center"><img src="vendor/images/template/fepacoc-crescimento.png" height="200" alt=""></div>
+                <h2 class="text-center">Crescimento Projetado</h2>
+                <p class="text-center ">O FEPACOC será uma bússola para organizar seu crescimento, aplicar estratégias de marketing assertivas, aumentar a eficiência operacional, construir a cultura da sua empresa e gerar a margem de lucro desejada. </p>
+            </div>
+        </section>
+        <section class="scroll-section metodo-secao cinza" id="section6">
+            <div class="row">
+                <div class="col-md-5 mb-4 mx-auto">
+                    <div class="card-body p-0">
+                        <div id="lineChart"></div>
+                    </div>
+                </div>
+                <div class="col-md-5 mx-auto ">
+                    <h2 class="text-center">Veja onde sua empresa pode chegar <b class="text-primary">ao conhecer seus indicadores</b>.</h2>
+                    <p class="text-center ">Não é mágica. O FEPACOC analisa os dados chave da sua empresa e os transforma em pequenas metas diárias, simples e alcançáveis. Ao cumprir essas metas, você direciona sua empresa para um crescimento exponencial e sustentável. </p>
+                </div>
+            </div>
+        </section>         
+        <section class="scroll-section hero  text-center text-white" id="section8">
+            <div class="container">
+                <h1 class="display-4 mt-5">Não é curso ou apenas teoria. <b class="text-info">É aplicação prática!</b></h1>
+                <p class="lead mb-4">Entendemos que você não tem tempo para fazer um curso de gestão e análise, <span class="badge bg-orange text-blue-fg">você quer resultado</span>. O Fepacoc foi projetado justamente para você, deixamos a teoria para segundo plano, entramos na pratica já no primeiro dia, organizamos as informações e deixamos pronto para você apenas aplicar.</p>
+                <div class="row">
+                    <div class="col-sm-4 mx-auto">
+                        <a href="planos"  class="btn btn-secondary btn-lg w-100 mt-5 py-2 mb-8">
+                            Conhecer Mais
+                        </a>
+                    </div>
+                    <div class="col-sm-4 mx-auto">
+                        <a href="planos" id="cadastro-btn" class="btn btn-info btn-lg btn-cadastro w-100 mt-5 py-2 mb-8">
+                            Ver os Preços
+                        </a>
+                    </div>                
+                </div>
+                
+            </div>
+        </section>
+        <!-- <section class="scroll-section metodo-secao cinza" id="section7">
+            <div class="container">
+                <div class="col-sm-6 mx-auto">
+                    <h2 class="text-center">Planos Disponíveis</h2>
+                    <hr>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card p-4 h-100 shadow">
+                            <div class="ribbon ribbon-top bg-secondary"></div>
+                            <h4 class="text-center">Estratégia</h4>
+                            <p class="text-secondary text-center">Este é o plano que te ensina como aplicar a estratégia FEPACOC. Organize sua empresa e construa o plano futuro.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card p-4 h-100 shadow">
+                            <div class="ribbon ribbon-top bg-secondary"></div>
+                            <h4 class="text-center">Análise</h4>
+                            <p class="text-secondary text-center">Ideal para MEIs e empresas que faturam até 50mil. Use a ferramenta exclusiva do FEPACOC para crescer organizado.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card p-4 h-100 shadow">
+                            <div class="ribbon ribbon-top bg-secondary"></div>
+                            <h4 class="text-center">Crescimento e Aceleração</h4>
+                            <p class="text-secondary text-center">Plano ajustado para empresas com grande potêncial, trabalhe junto com o time FEPACOC e alcance seus melhores resultado.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section> -->
+        <section class="scroll-section metodo-secao  text-center" id="section9">
+            <div class="container">
+                <p><strong>"A análise de dados é a chave para prover o crescimento e melhorar seus processos."</strong><br>— FEPACOC</p>
+            </div>
+        </section>
+    
+    </div>
+    <!-- Libs JS -->
+    <?php include "app/public/graficos/g-kpis.php" ?>
+
+    <script src="vendor/libs/apexcharts/dist/apexcharts.min.js" defer></script>
+    <!-- Tabler Core -->
+    <script src="vendor/js/tabler.min.js" defer></script>
+    <script src="vendor/js/demo.min.js" defer></script>
+
+   
+</body>
+
+</html>
+
--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
+session_start();
+date_default_timezone_set('America/Sao_Paulo');
+// Verifica se o token de acesso está na URL
+if(isset($_GET['pg']) && $_GET['pg'] != ""){ 
+    $page = $_GET['pg'];
+    $page_public = "app/public/{$page}.php";
+    $page_private = "app/private/{$page}.php";
 
-print_r($_GET);
+    //logout do usuário
+    if($page == 'logout'){
+        include "logout.php";
+    }
 
-?>
+    if(file_exists($page_public)) {        
+        include "app/public/{$page}.php";        
+    }
+    if(file_exists($page_private)) { 
+        if(isset($_SESSION['cliente_info']['user_info'][0])) {
+            include "app/private/{$page}.php";
+        }else{
+            header('Location: login');
+        }
+        
+    }
+}else{
+    include "app/public/inicio.php";
+}
