2e3815124636bcd4d643acb4a262ca4e1c103145
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-02-20 16:52:41 -0300

    1

--- app/private/parts/botoes-menu.php
+++ app/private/parts/botoes-menu.php
@@ -7,7 +7,11 @@
                       Minhas Análises
             </a>
             <a href="painel&a=treinamento" class="text-secondary m-1">
-                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icon-tabler-wrench text-info fs-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11 6h9" /><path d="M11 12h9" /><path d="M11 18h9" /><path d="M4 10v-4.5a1.5 1.5 0 0 1 3 0v4.5" /><path d="M4 8h3" /><path d="M4 20h1.5a1.5 1.5 0 0 0 0 -3h-1.5h1.5a1.5 1.5 0 0 0 0 -3h-1.5v6z" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-wrench text-info" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M13.21 13.21l-3.42 3.42a1.5 1.5 0 0 1 -2.12 0l-3.42 -3.42a1.5 1.5 0 0 1 0 -2.12l3.42 -3.42" />
+                    <path d="M14.83 9.17a4 4 0 1 1 0 5.66" />
+                    </svg>
                         Ferramentas
             </a>
             <a href="painel&a=treinamento" class="text-secondary m-1">
