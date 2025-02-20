6a0cf51fce1b07036587420270c139f0acc2e75b
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-02-20 17:16:48 -0300

    1

--- app/private/paginas/empresas/inicial-empresas.php
+++ app/private/paginas/empresas/inicial-empresas.php
@@ -1,1 +1,23 @@
-Inicial Empresas
+<div class="page-body">
+    <div class="container-xl">
+        <div class="col-sm-12 p-2 ">
+            <div class="row">
+                <div class="card card-sponsor col-md-7 ">
+                    <div class="row m-auto">
+                        <div class="col-sm-5">
+                            <img src="vendor/images/relatorio/magica.png" alt="">
+                        </div>
+                        <div class="col m-auto">
+                            <h1 class="text-start m-auto"><b class="text-info">Update</b><br> Faça o update do seu plano e gerencie multiplas empresas.</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">                    
+                        
+                            
+                        
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
