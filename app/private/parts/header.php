f643d375b7939cb4549a4aa8251958c908e6dd94
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-02-20 15:48:47 -0300

    1

--- app/private/parts/header.php
+++ app/private/parts/header.php
@@ -9,7 +9,7 @@
           <img src="vendor/images/fepacoc/logobranco_fepacoc.png" width="110" height="32" alt="Logo" class="navbar-brand-image mt-2">
         </a>
       </h1>
-      <div class="btn-list d-none d-lg-inline-block">
+      <div class="btn-list">
           <a href="dashboard&setting=minhas-empresas" class="btn list-group-item list-group-item-action" class="list-group-item list-group-item-action" aria-current="true">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-category text-info"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 4h6v6h-6z" /><path d="M14 4h6v6h-6z" /><path d="M4 14h6v6h-6z" /><path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /></svg>
                       Minhas Empresas
