2e4a3cbd719b64ea9073112ac5698d5c28a2ee41
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-02-20 14:29:07 -0300

    1

--- app/private/paginas/inicial-painel.php
+++ app/private/paginas/inicial-painel.php
@@ -1,5 +1,60 @@
 <div class="page-body">
     <div class="container-xl">
+        <div class="col-sm-12 p-2 mt-6">
+            <div class="row">
+                <div class="card card-sponsor col-md-7 ">
+                    <div class="row m-auto">
+                        <div class="col-sm-5">
+                            <img src="vendor/images/relatorio/magica.png" alt="">
+                        </div>
+                        <div class="col m-auto">
+                            <h1 class="text-start m-auto"><b class="text-info">Não é mágica!</b><br> É apenas a organização e cálculo dos seus dados para impulsionar seu crescimento.</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="card p-2">
+                        <h3 class=" text-center mt-3">Acessos</h3>
+                        <div class="list-group list-group-flush">
+                            <div class="row mt-2">
+                                <div class="col-6">
+                                    <a href="dashboard&setting=minhas-empresas" class="btn list-group-item list-group-item-action
+                                        <?php if (isset($_GET['setting']) && $_GET['setting'] == "minhas-empresas") {echo 'btn-outline-cyan';} ?>" class="list-group-item list-group-item-action" aria-current="true">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-category text-info"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 4h6v6h-6z" /><path d="M14 4h6v6h-6z" /><path d="M4 14h6v6h-6z" /><path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /></svg>
+                                        Minhas Empresas
+                                    </a>
+                                </div>
+                                <div class="col-6">
+                                    <a href="#" class="btn list-group-item list-group-item-action ">
+                                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chart-bar text-info"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 13a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M15 9a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M9 5a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M4 20h14" /></svg>
+                                    Minhas Análises</a>
+                                </div>
+                                <div class="col-6">
+                                    <a href="#" class="btn list-group-item list-group-item-action ">
+                                         <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-list-letters text-info"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11 6h9" /><path d="M11 12h9" /><path d="M11 18h9" /><path d="M4 10v-4.5a1.5 1.5 0 0 1 3 0v4.5" /><path d="M4 8h3" /><path d="M4 20h1.5a1.5 1.5 0 0 0 0 -3h-1.5h1.5a1.5 1.5 0 0 0 0 -3h-1.5v6z" /></svg>
+                                    Treinamento</a>
+                                </div>
+                                <div class="col-6">
+                                    <a href="#" class="btn list-group-item list-group-item-action ">
+                                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-player-play text-info"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 4v16l13 -8z" /></svg>
+                                    Aulas Exclusivas</a>
+                                </div>
+                                <!-- <div class="col-6">
+                                    <a class="btn list-group-item list-group-item-action" data-bs-toggle="offcanvas" href="#offFinanceiro" role="button" aria-controls="offcanvasStart">
+                                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-file-dollar text-info"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" /><path d="M14 11h-2.5a1.5 1.5 0 0 0 0 3h1a1.5 1.5 0 0 1 0 3h-2.5" /><path d="M12 17v1m0 -8v1" /></svg>
+                                    Financeiro</a>
+                                </div> -->
+                                <div class="col-6">
+                                    <a class="btn list-group-item list-group-item-action" data-bs-toggle="offcanvas" href="#offNotifica" role="button" aria-controls="offcanvasStart">
+                                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-bell-ringing text-info"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" /><path d="M9 17v1a3 3 0 0 0 6 0v-1" /><path d="M21 6.727a11.05 11.05 0 0 0 -2.794 -3.727" /><path d="M3 6.727a11.05 11.05 0 0 1 2.792 -3.727" /></svg>
+                                         Notificação </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         
         <div class="col-sm-12 mt-4">
             <h3 class="text-secondary text-center"> Disponível para gerenciamento </h3>
