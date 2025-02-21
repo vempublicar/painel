49a6025dc842406c94535753c2da8cd8646ea50e
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-02-21 16:53:16 -0300

    1

--- app/private/parts/header.php
+++ app/private/parts/header.php
@@ -108,77 +108,52 @@
 				<div class="container-xl">
 					<div class="row flex-fill align-items-center">
 						<div class="col">
-<ul class="navbar-nav">
-		<li class="nav-item active">
-			<a class="nav-link" href="./#">
-				<span class="nav-link-icon d-md-none d-lg-inline-block">
-	<!-- Download SVG icon from http://tabler.io/icons/icon/star -->
-	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1"><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path></svg>
-</span>
-				<span class="nav-link-title">
-					First
-				</span>
-			</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="./#">
-				<span class="nav-link-icon d-md-none d-lg-inline-block">
-	<!-- Download SVG icon from http://tabler.io/icons/icon/star -->
-	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1"><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path></svg>
-</span>
-				<span class="nav-link-title">
-					Second
-				</span>
-				<span class="badge badge-sm bg-red">2</span>
-			</a>
-		</li>
-		<li class="nav-item dropdown">
-			<a class="nav-link dropdown-toggle" href="#navbar-third" data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button" aria-expanded="false">
-				<span class="nav-link-icon d-md-none d-lg-inline-block">
-	<!-- Download SVG icon from http://tabler.io/icons/icon/star -->
-	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1"><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path></svg>
-</span>
-				<span class="nav-link-title">
-					Third
-				</span>
-			</a>
-			<div class="dropdown-menu">
-					<a class="dropdown-item" href="./#">
-						First
-					</a>
-					<a class="dropdown-item" href="./#">
-						Second
-					</a>
-					<a class="dropdown-item" href="./#">
-						Third
-					</a>
-				</div>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link disabled" href="./#">
-				<span class="nav-link-icon d-md-none d-lg-inline-block">
-	<!-- Download SVG icon from http://tabler.io/icons/icon/star -->
-	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1"><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path></svg>
-</span>
-				<span class="nav-link-title">
-					Disabled
-				</span>
-			</a>
-		</li>
-</ul>
-						</div>
-						<div class="col-2 d-none d-xxl-block">
-<div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last">
-	<form action="./" method="get" autocomplete="off" novalidate="">
-<div class="input-icon">
-		<span class="input-icon-addon">
-	<!-- Download SVG icon from http://tabler.io/icons/icon/search -->
-	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1"><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path><path d="M21 21l-6 -6"></path></svg>
-		</span>
-	<input type="text" value="" class="form-control" placeholder="Search…" aria-label="Search in website">
-</div>
-	</form>
-</div>
+                  <ul class="navbar-nav">
+                      <li class="nav-item active">
+                        <a class="nav-link" href="./#">
+                          
+                          <span class="nav-link-title">
+                            First
+                          </span>
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="./#">
+                          
+                          <span class="nav-link-title">
+                            Second
+                          </span>
+                          <span class="badge badge-sm bg-red">2</span>
+                        </a>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#navbar-third" data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button" aria-expanded="false">
+                          
+                          <span class="nav-link-title">
+                            Third
+                          </span>
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="./#">
+                              First
+                            </a>
+                            <a class="dropdown-item" href="./#">
+                              Second
+                            </a>
+                            <a class="dropdown-item" href="./#">
+                              Third
+                            </a>
+                          </div>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link disabled" href="./#">
+                          
+                          <span class="nav-link-title">
+                            Disabled
+                          </span>
+                        </a>
+                      </li>
+                  </ul>
 						</div>
 					</div>
 				</div>
