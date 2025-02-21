30e3c2c1e8071f74fd220df7676ffd8755d75109
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-02-21 14:41:30 -0300

    1

--- app/private/parts/header.php
+++ app/private/parts/header.php
@@ -1,4 +1,4 @@
-  <header class="navbar d-print-none header1">
+  <header class="navbar d-print-none header1 p-2">
     <div class="container-xl">
       <h1 class="d-none-navbar-horizontal">
         <a href="#" id="logo-branco">
