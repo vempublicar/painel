2f5ed6404010e3f1f445016e2bef0ea6df02e1b7
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-02-21 10:06:22 -0300

    1

--- app/private/paginas/empresas/inicial-empresas.php
+++ app/private/paginas/empresas/inicial-empresas.php
@@ -36,17 +36,14 @@ if(empty($minhas_empresas)) {
     <div class="container-xl">
         <div class="col-sm-12 p-2 ">
             <div class="row">
-                <div class="card card-sponsor col-md-7 ">
+                <div class="card card-sponsor col-md-4 ">
                     <div class="row m-auto">
-                        <div class="col-sm-5">
-                            <img src="vendor/images/relatorio/magica.png" alt="">
-                        </div>
                         <div class="col m-auto">
                             <h1 class="text-start m-auto"><b class="text-info">Update</b><br> Faça o update do seu plano e gerencie multiplas empresas.</h1>
                         </div>
                     </div>
                 </div>
-                <div class="card col-md-5">
+                <div class="card col-md-8">
                     <div class="card-body">
                         <?php if (!empty($minhas_empresas)): ?>
                             <ul class="list-group">
