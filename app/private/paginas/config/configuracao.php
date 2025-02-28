d1e58aa5377158f23d20608c49bde46eeccf40f8
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-02-28 17:51:05 -0300

    1

--- app/private/paginas/config/configuracao.php
+++ app/private/paginas/config/configuracao.php
@@ -1,4 +1,20 @@
+<?php
+$minhas_empresas = $_SESSION['minhas_empresas'];
+if (isset($_GET['c'])) {
+    $empresa_edit = base64_decode($_GET['c']);
+    $cargo = '';
+    $id = '';
 
+    foreach ($minhas_empresas as $empresa) {
+        if ($empresa['cnpj'] === $empresa_edit) {
+            $empresaEditada = $empresa;
+            break;  // Interrompe o loop uma vez que a empresa correspondente é encontrada
+        }
+    }
+
+}
+print_r($empresa_edit);
+?>
 <div class="page-body">
   <div class="container-xl">
     <div class="card ">
