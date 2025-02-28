ebfca34c8a133f5daebe10a4774d130395a30ea8
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-02-28 17:49:40 -0300

    1

--- app/private/paginas/config/conta.php
+++ app/private/paginas/config/conta.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-sm-6">
           <h3>Detalhes da Conta</h3>
-          <?php print_r($empresaEditada) ?>
+          
           <p><strong>Nome da Empresa:</strong> <?= $empresaEditada['nome_empresa'] ?></p>
           <p><strong>CNPJ:</strong> <?= $empresaEditada['cnpj'] ?></p>
           <p><strong>CEP:</strong> <?= $empresaEditada['cep'] ?></p>
