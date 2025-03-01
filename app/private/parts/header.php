28bb2e7670041910ce44b9828d2c20d76a437045
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-03-01 00:25:29 -0300

    1

--- app/private/parts/header.php
+++ app/private/parts/header.php
@@ -229,7 +229,7 @@
                       <li class="nav-item">
                         <a class="nav-link" href="painel&a=edit-empresa&b=analise&c=<?= $_GET['c'] ?>">                          
                           <span class="nav-link-title">
-                            Análise
+                            Análises
                           </span>
                         </a>
                       </li>
@@ -257,6 +257,5 @@
   <?php if (isset($_GET['msg'])): ?>
             <div class="alert alert-warning alert-dismissible fade show" role="alert">
                 <?= htmlspecialchars(urldecode($_GET['msg'])) ?>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
     <?php endif; ?>
