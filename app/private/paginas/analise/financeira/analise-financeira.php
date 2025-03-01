d1517420040bff1bfabc7b1461e7ef6e64f0a554
Vem Publicar <127986394+vempublicar@users.noreply.github.com>
2025-03-01 17:32:55 -0300

    1

---
+++ app/private/paginas/analise/financeira/analise-financeira.php
@@ -0,0 +1,162 @@
+                
+                
+                <div class="card bg-white ">
+                    <div class="row">
+                        <div class="col-sm-5 ">
+                            <div class="card-body">
+                                <div class="empty">
+                                    <div><img src="vendor/images/relatorio/equilibrar-contas.jpg" height="200" alt="">
+                                    </div>
+                                    <p class="empty-title text-primary">"Os números ainda não são bons, mas há espaço para recuperação."</p>
+                                </div>
+                                <div class="row">
+                                    <div class="col mx-auto">
+                                        <div class="col">
+                                            <div class="font-weight-medium text-center">
+                                                <span class="h3 font-weight-medium text-yellow text-center">R$0,00</span>
+                                            </div>
+                                            <div class="text-secondary text-center">
+                                                <b>Fechamento do Mês</b>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col mx-auto">
+                                        <div class="col" title="">
+                                            <div class="font-weight-medium text-center">
+                                                <span class="h3 font-weight-medium text-yellow text-center">0%</span>
+                                            </div>
+                                            <div class="text-secondary text-center">
+                                                <b>Crescimento da Empresa</b>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-7 m-auto">
+                            <div class="row row-deck row-cards m-5 ">
+                                <div class="col-sm-4 text-center">
+                                    <div class="card bg-white card-sm">
+                                        <div class="card-status-start bg-yellow"></div>
+                                        <div class="card-body">
+                                            <div class="row align-items-center">
+                                                <div class="col">
+                                                    <div class="h2 font-weight-medium text-yellow ">
+                                                        0%                                                    </div>
+                                                    <div class="text-secondary">
+                                                        <b>CMV</b>
+                                                    </div>
+                                                    <div class="text-secondary">
+                                                        Lucro Bruto
+                                                    </div>
+                                                    <i class="text-secondary">(R$0,00)</i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4 text-center">
+                                    <div class="card bg-white card-sm">
+                                        <div class="card-status-start bg-yellow"></div>
+                                        <div class="card-body">
+                                            <div class="row align-items-center">
+                                                <div class="col">
+                                                    <div class="h2 font-weight-medium text-yellow ">
+                                                        0%                                                    </div>
+                                                    <div class="text-secondary">
+                                                        <b>EBIT</b>
+                                                    </div>
+                                                    <div class="text-secondary">
+                                                        Lucro Operacional
+                                                    </div>
+                                                    <i class="text-secondary">(R$0,00)</i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4 text-center">
+                                    <div class="card bg-white card-sm">
+                                        <div class="card-status-start bg-yellow"></div>
+                                        <div class="card-body">
+                                            <div class="row align-items-center">
+                                                <div class="col">
+                                                    <div class="h2 font-weight-medium text-yellow ">
+                                                        0%                                                    </div>
+                                                    <div class="text-secondary">
+                                                        <b>MLL</b>
+                                                    </div>
+                                                    <div class="text-secondary">
+                                                        Margem de Lucro Líquido
+                                                    </div>
+                                                    <i class="text-secondary">(R$0,00)</i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4 text-center">
+                                    <div class="card bg-white card-sm">
+                                        <div class="card-status-start bg-yellow"></div>
+                                        <div class="card-body">
+                                            <div class="row align-items-center">
+                                                <div class="col">
+                                                    <div class="h2 font-weight-medium text-yellow ">
+                                                        0%                                                    </div>
+                                                    <div class="text-secondary">
+                                                        <b>RCIP</b>
+                                                    </div>
+                                                    <div class="text-secondary">
+                                                        Retorno de Capital Investido
+                                                    </div>
+                                                    <i class="text-secondary">(R$0,00)</i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4 text-center">
+                                    <div class="card bg-white card-sm">
+                                        <div class="card-status-start bg-yellow"></div>
+                                        <div class="card-body">
+                                            <div class="row align-items-center">
+                                                <div class="col">
+                                                    <div class="h2 font-weight-medium text-yellow ">
+                                                        0%                                                    </div>
+                                                    <div class="text-secondary">
+                                                        <b>FCL</b>
+                                                    </div>
+                                                    <div class="text-secondary">
+                                                        Fluxo de Caixa Livre
+                                                    </div>
+                                                    <i class="text-secondary">(R$0,00)</i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4 text-center">
+                                    <div class="card bg-white card-sm">
+                                        <div class="card-status-start bg-yellow"></div>
+                                        <div class="card-body">
+                                            <div class="row align-items-center">
+                                                <div class="col">
+                                                    <div class="h2 font-weight-medium text-yellow ">
+                                                        0%                                                    </div>
+                                                    <div class="text-secondary">
+                                                        <b>ARP</b>
+                                                    </div>
+                                                    <div class="text-secondary">
+                                                        Ativos e Receitas Previsiveis
+                                                    </div>
+                                                    <i class="text-secondary">(R$0,00)</i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
